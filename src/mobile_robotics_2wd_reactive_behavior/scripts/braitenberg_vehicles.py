20edbcbd4bd115579e26dea71328e7c18e5fe5ca
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-11-17 20:00:34 +0000

    setup braitenberg node

---
+++ src/mobile_robotics_2wd_reactive_behavior/scripts/braitenberg_vehicles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+''' This node implements the basic behavior algorithms of
+mobile robots based on the Bratenberg Vehicles as presented
+in the book Elements of Robotics by Mordechai Ben-Ari and Francescq Mondada
+'''
+import rospy
+import sys
+
+
+behaviors = {}
+
+
+if __name__ == '__main__':
+    pass
